de7cd5f0fcd9a0530d610e5082f31e3bd7e8643d
Hendrik Visage <hvisage@envisage.co.za>
2021-04-28 01:28:09 +0200

    "working" changes

--- zfs-root/zfs-root.sh
+++ zfs-root/zfs-root.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#Copy righted BSD
-set -x
+#Copyrighted BSD (Or whatever but updates and assistance to get it into Debian would be great ;)
+#
+# wget https://apt.tros.ovh/scripts/zfs-root.sh
+set +x
 id -a
+WAIT=echo
 
+echo "Need some contrib stuff and ZFS"
+echo "-------------------------------"
 cat > /etc/apt/sources.list << EOF
 deb http://deb.debian.org/debian/ buster main contrib non-free
 deb http://deb.debian.org/debian/ buster-updates main contrib non-free
@@ -11,16 +16,22 @@ deb http://deb.debian.org/debian buster-backports main contrib non-free
 EOF
 
 apt -y update
-apt install --yes debootstrap gdisk dkms dpkg-dev dosfstools curl jq pciutils wget linux-headers-$(uname -r)
+apt -y full-upgrade
+apt install --yes debootstrap gdisk dkms dpkg-dev dosfstools curl jq pciutils wget rsync linux-headers-$(uname -r)
+hash
+
+#Accept the license agreements:
 echo zfs-dkms zfs-dkms/note-incompatible-licenses note  | debconf-set-selections
 apt install --yes -t buster-backports --no-install-recommends zfs-dkms
-
 apt install --yes -t buster-backports zfsutils-linux
 modprobe zfs
-which curl
-read "next"
+which curl jq rsync
 
-ROOTDISK=`lsblk --json | jq '.blockdevices[]|select (.children[].mountpoint == "/")|.name'` ; echo $ROOTDISK
+${WAIT} "next"
+
+echo "Find the disk we booted from"
+ROOTDISK=`lsblk --json | jq  '.blockdevices[]|select (.type == "disk" and has("children"))| select (.children[].mountpoint == "/")|.name'` ; echo $ROOTDISK
+echo "Now get the rest of the disks to add to ZFS"
 OTHERDISKS=`lsblk --output-all --json | jq -r '.blockdevices[]| select (.name != '$ROOTDISK')|(.model+" "+.serial) '| tr ' ' '_'` ; echo $OTHERDISKS
 
 pushd /dev/disk/by-id
@@ -31,67 +42,78 @@ done
 echo $DISKNAMES
 popd
 
+echo "format the disk for ZFS"
 DUMMYSIZE=0
 for D in $DISKNAMES
 do
 	DISK=/dev/disk/by-id/$D
 	sgdisk --zap-all $DISK
+	#We might not need this:
 	sgdisk -a1 -n1:24K:+1000K -t1:EF02 $DISK
 	sgdisk     -n2:1M:+1024M   -t2:EF00 $DISK
 	sgdisk     -n3:0:0        -t3:BF00 $DISK
 	partx -s $DISK
+	#udev needs to get it's stuff done, else the format doesn't
 	udevadm settle
-	mkfs.vfat -F32 -n EFI ${DISK}-part2
+	#mkfs.vfat -F32 -n EFI ${DISK}-part2
 	P3S=`lsblk -nbo SIZE ${DISK}-part3`
+	#This is needed to get the size for the dummy disk
 	if [ $DUMMYSIZE -lt $P3S ] ; then DUMMYSIZE=$P3S ; fi
-
 	MEMBERS=${D}-part3" "$MEMBERS
-
 done
 
+#Dummy disk
 truncate -s $DUMMYSIZE /tmp/DUMMY
 MEMBERS=$MEMBERS" "/tmp/DUMMY
 lsblk
 
-read "next"
+${WAIT} "next"
 TYPE=mirror
 
 zpool create -f -o ashift=12 -O acltype=posixacl -O canmount=off -O compression=lz4 \
     -O dnodesize=auto -O normalization=formD -O relatime=on \
     -O xattr=sa -O mountpoint=/ -R /mnt \
     rpool $TYPE $MEMBERS
+#offline the dummy as to prevent disk writes to it
 zpool offline rpool /tmp/DUMMY
-read "next"
+zpool status -v
+${WAIT} "next"
 
+#This creates the necessary mountpoints
 zfs create -o canmount=off -o mountpoint=none rpool/ROOT
 zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/debian10
 zfs mount rpool/ROOT/debian10
-
 zfs create                                 rpool/boot
 mkdir /mnt/boot/efi
-mount ${DISK}-part2 /mnt/boot/efi
-
+#mount ${DISK}-part2 /mnt/boot/efi
 zfs create                                 rpool/home
 zfs create -o mountpoint=/root             rpool/home/root
 chmod 700 /mnt/root
 zfs create -o canmount=off                 rpool/var
 zfs create -o com.sun:auto-snapshot=false    -o mountpoint=/var/lib/docker rpool/docker
 zfs create                                 rpool/var/log
-
 zpool status -v
 zfs list
-read "next"
+${WAIT} "next"
+
+#Install Debian in the ZFS rpool
+debootstrap  --exclude=apparmor \
+  --include=efivar,efibootmgr,dosfstools,gnupg2,ca-certificates,\
+ca-certificates-java,ca-certificates-mono,curl,wget,gdisk,util-linux,\
+jq,sudo,openssh-server,dpkg-dev,linux-headers-amd64,linux-image-amd64 \
+  buster /mnt || exit 111
 
 
-debootstrap  --include=efivar,efibootmgr,dosfstools,gnupg2,ca-certificates,ca-certificates-java,ca-certificates-mono,curl,wget buster /mnt
-
+#fixing ZFS related stuff
 mkdir -p /mnt/etc/apt/sources.list.d/
 cp /etc/apt/sources.list.d/* /mnt/etc/apt/sources.list.d/
 mkdir /mnt/etc/zfs
 cp /etc/zfs/zpool.cache /mnt/etc/zfs/
-apt install rsync
-hash
-rsync -vaP /etc/network /mnt/etc
+
+echo
+echo '# Need the "Same" network'
+echo
+rsync -va /etc/network /mnt/etc
 
 cat > /mnt/etc/apt/sources.list << EOF
 deb http://deb.debian.org/debian/ buster main contrib non-free
@@ -107,12 +129,11 @@ Pin-Priority: 990
 EOF
 
 pushd /mnt/root
-
 mkdir -p pubkeys debs scripts
-
 curl  https://last-public-ovh-rtm.snap.mirrors.ovh.net/ovh_rtm.pub > pubkeys/ovh_rtm.pub
 curl  http://last.public.ovh.metrics.snap.mirrors.ovh.net/pub.key > pubkeys/ovh_metrics.pub
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc > pubkeys/pg.pub
+
 wget --directory-prefix=`pwd`/debs/ https://apt.tros.ovh/debs/hev-kernel-helper_1.0.0+1_all.deb
 wget --directory-prefix=`pwd`/debs/ https://apt.tros.ovh/debs/kernel-4.19_1.0.0+1_all.deb
 cat > debconf.settings << EOF
@@ -121,44 +142,83 @@ console-setup   console-setup/fontface47        select  Do not change the boot/k
 console-setup   console-setup/charmap47 select  UTF-8
 keyboard-configuration  keyboard-configuration/unsupported_config_layout        boolean true
 keyboard-configuration  keyboard-configuration/layoutcode       string  us
+keyboard-configuration  keyboard-configuration/variant select English (US)
 locales locales/default_environment_locale      select  C.UTF-8
 locales locales/locales_to_be_generated multiselect     en_US.UTF-8 UTF-8, en_ZA ISO-8859-1, en_ZA.UTF-8 UTF-8
 zfs-dkms zfs-dkms/note-incompatible-licenses note
 EOF
-
-read "next2"
+# lsblk --json -O --list |jq -r '.blockdevices[]|select (.label =="EFI")|.uuid' > /mnt/etc/kernel/efiboot-uuids
+echo ""
+${WAIT} "--next2---"
 
 cat > script.sh << EOFM
 #!/bin/bash
 set -x
 cd /root
-apt install --yes gnupg2 ca-certificates ca-certificates-java ca-certificates-mono curl
+
 cat /root/pubkeys/* | apt-key add -
 
 debconf-set-selections < /root/debconf.settings
 apt update
 apt upgrade --yes
-apt update
 apt install --yes console-setup locales
 update-locale LANG=en_US.UTF-8
-apt-get install --yes ovh-rtm-metrics-toolkit
-
-apt install --yes dpkg-dev linux-headers-amd64 linux-image-amd64
 apt install --yes zfs-initramfs
+apt install --yes ovh-rtm-metrics-toolkit
 
+echo
+echo "**** done apt-gets"
+echo
+dpkg -i /root/debs/hev-kernel*
+echo $DISKNAMES
+echo
+echo "%%%%%%% efi jobs"
+echo
+for D in $DISKNAMES
+do
+ /usr/sbin/hev-efiboot-tool format /dev/disk/by-id/\${D}-part2 --force
+ echo "waiting for them udev..." ; sleep 2
+ /usr/sbin/hev-efiboot-tool init /dev/disk/by-id/\${D}-part2
+done
+/usr/sbin/hev-efiboot-tool clean
+/usr/sbin/hev-efiboot-tool kernel list
+/usr/sbin/hev-efiboot-tool refresh
+#bootctl install --path=/boot/efi
 
-
-bootctl install --path=/boot/efi
-
-set +x
 EOFM
 
 chmod +x script.sh
 popd
 
+echo "
+Add debian user
+"
+useradd -R /mnt -m -s /bin/bash debian
+rsync -va ~debian/.ssh /mnt/home/debian
+echo
+echo "====extr etc stuff"
+echo
+rsync -va /etc/ssh /etc/sudo* /mnt/etc
+rsync -va ~/.ssh /mnt/root
+
+echo "root=ZFS=rpool/ROOT/debian10 boot=zfs" > /mnt/etc/kernel/cmdline
+
+echo
+echo "=====Mounting devices==="
+echo
 mount --rbind /dev  /mnt/dev
 mount --rbind /proc /mnt/proc
 mount --rbind /sys  /mnt/sys
+mount --rbind /run  /mnt/run
 
-chroot /mnt /usr/bin/env DISK=$DISK bash /root/script.sh
+echo
+echo "----chroot---"
+echo
+chroot /mnt /usr/bin/env bash /root/script.sh
+echo
+echo "===done chroot script.sh"
+echo
+umount -flvR /mnt
+zpool export rpool
+reboot -f
 
